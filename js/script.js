2ca120137dc55366959fad02c587d2fce048065f
mrpribadi <didi@datascrip.co.id>
2024-04-25 15:27:25 +0700

    update dark mode dan perbaikan beberapa fitur

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const backToTop = document.querySelector("#back-to-top");
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    backToTop.classList.remove("hidden");
+    backToTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    backToTop.classList.remove("flex");
+    backToTop.classList.add("hidden");
   }
 };
 
@@ -15,3 +20,35 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
